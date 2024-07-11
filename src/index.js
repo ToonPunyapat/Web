e1f8b67b7ea193cb3ae4a2b4466c0366df169bfc
ToonPunyapat <toonpunyapat@gmail.com>
2024-07-11 23:42:42 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Create from "./pages/Create";
 import Search from "./pages/Search";
+import Contact from "./pages/Contact";
 
 const router = createBrowserRouter([
   {
@@ -20,9 +21,16 @@ const router = createBrowserRouter([
     path: "/search",
     element: <Search />,
   },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
