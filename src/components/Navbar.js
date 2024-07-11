9098b10769d189a372a2e3c9665b9b6be1fbc930
ToonPunyapat <toonpunyapat@gmail.com>
2024-07-11 23:43:12 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,57 +3,35 @@ import React from "react";
 function Navbar() {
   return (
     <div>
-      <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <div class="container-fluid">
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarTogglerDemo01"
-              aria-controls="navbarTogglerDemo01"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-              <a class="navbar-brand" href="#">
-                Hidden brand
-              </a>
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Link
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" aria-disabled="true">
-                    Disabled
-                  </a>
-                </li>
-              </ul>
-              <form class="d-flex" role="search">
-                <input
-                  class="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                >
-                  <button class="btn btn-outline-success" type="submit">
-                    Search
-                  </button>
-                </input>
-              </form>
-            </div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+        <div className="container-fluid   ">
+          <span className="navbar-brand">Note Easy</span>
+          <div className="collapse navbar-collapse  ">
+            <ul className="navbar-nav nav-underline ">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/create">
+                  Create
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/search">
+                  Search
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </div>
   );
 }
